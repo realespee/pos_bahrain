7c5a42959fa9c6502f36c8f0e1eae4e5bce19f37
realespee <wanyamasp@gmail.com>
2022-04-22 10:45:24 +0300

    fetch total valuation data

--- pos_bahrain/pos_bahrain/report/branch_stock/branch_stock.py
+++ pos_bahrain/pos_bahrain/report/branch_stock/branch_stock.py
@@ -90,7 +90,6 @@ def _get_data(clauses, values, keys):
                 b.projected_qty AS qty,
                 w.branch AS branch
             FROM `tabBin` AS b
-            LEFT JOIN `tabBranch` AS w ON w.warehouse = b.warehouse
         """
     )
 
