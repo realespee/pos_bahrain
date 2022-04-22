4d34181330dcb50e28a8a6de23dce645e2b23369
realespee <wanyamasp@gmail.com>
2022-04-22 09:23:24 +0300

    fetch total valuation data

--- pos_bahrain/pos_bahrain/report/branch_stock/branch_stock.py
+++ pos_bahrain/pos_bahrain/report/branch_stock/branch_stock.py
@@ -55,7 +55,7 @@ def _get_columns(filters):
         [make_column(x, x, type="Float", width=90) for x in branches],
         [make_column("total_qty", "Total Qty", type="Float", width=90)],
         [make_column("valuation_rate", "Valuation Rate", type="Currency", width=90)],
-        [make_column('total_qty * valuation_rate', "Total Valuation", type="Currency", width=90)]
+        [make_column('total_valuation', "Total Valuation", type="Currency", width=90)]
     )
 
 
